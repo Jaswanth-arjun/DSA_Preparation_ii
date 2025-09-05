11a0cee6ac4e05d74935ff13e8e968177fd5ab51
Jaswanth-arjun <138548908+Jaswanth-arjun@users.noreply.github.com>
2025-09-05 15:24:51 +0530

    Pyramid.java

--- Pyramid.java
+++ Pyramid.java
@@ -22,7 +22,7 @@ Output    *
  import java.io.*;
 import java.util.*;
 
-public class PyramidPattern {
+public class Pyramid {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
